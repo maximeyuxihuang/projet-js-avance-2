64aa8ffafdf9334d5869261179c612392bebccfc
Maxime Huang <maximehuangyuxi@gmail.com>
2018-12-21 03:20:17 +0100

    add useref post add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import PostsList from './components/PostsList';
 import postsMock from './posts-mock';
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 
 const App = () => {
   const [data, setData] = useState(postsMock)
+  let titleRef = useRef()
+  let descriptionRef = useRef("")
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -18,9 +20,54 @@ const App = () => {
     })
   }, []);
 
+  const handleSend = () => {
+    let nextIndex = 0;
+    const userId = 1;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id >= nextIndex)
+        nextIndex = data[i].id + 1
+    }
+
+    const newElement = {
+      userId,
+      id: nextIndex,
+      title: titleRef.current.value,
+      body: descriptionRef.current.value
+    }
+    setData(data.concat([newElement]))
+    showMessage(false)
+    toggleButton(true)
+    setTimeout(() => {
+      showMessage(true)
+      toggleButton(false)
+      resetContent()
+    }, 3000)
+  }
+
+  const resetContent = () => {
+    titleRef.current.value = ""
+    descriptionRef.current.value = ""
+  }
+
+  const toggleButton = (disabled) => {
+    document.getElementById("sendButton").disabled = disabled
+  }
+
+  const showMessage = (visibility) => {
+    document.getElementById("message").hidden = visibility
+  }
+
   return (
     <div>
       <header className="App-header">
+        <div>
+          <input ref={titleRef} type="text" />
+          <input ref={descriptionRef} type="text" />
+          <button id="sendButton" type="button" onClick={handleSend}>
+            Submit
+          </button>
+          <text id="message" hidden="true">Successfully added new Post</text>
+        </div>
         <PostsList posts={data} />
       </header>
     </div>
